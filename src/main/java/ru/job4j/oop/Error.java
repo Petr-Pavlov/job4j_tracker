8a3be1b166ce0186fda7eadf2c5300d5b7793d5b
Petr-Pavlov <pavlov.p@hotmail.com>
2021-04-04 12:39:47 +0300

    1.7. Конструктор[173889#509035]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -5,18 +5,18 @@ public class Error {
     private int status;
     private String message;
 
-    public void printInfo() {
-        System.out.println("Активность: " + active);
-        System.out.println("Статус: " + status);
-        System.out.println("Сообщение: " + message);
-    }
-
     public Error() {}
 
     public Error (boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
     }
 
     public static void main(String[] args) {
