da5f9a8377a7b8b750154ec2bba7e174f425c87d
Petr-Pavlov <pavlov.p@hotmail.com>
2021-04-05 11:16:28 +0300

    4. Переопределение[174890#509041]

--- src/main/java/ru/job4j/Inheritance/JSONReport.java
+++ src/main/java/ru/job4j/Inheritance/JSONReport.java
@@ -3,6 +3,9 @@ package ru.job4j.Inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "name: " + name + System.lineSeparator() + "body: " + body;
+        return String.format("{" + System.lineSeparator()
+                + "name: %s," + System.lineSeparator()
+                + "body: %s"  + System.lineSeparator()
+                + "}", name, body);
     }
 }
